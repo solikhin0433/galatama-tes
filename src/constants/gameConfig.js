d7980ec9ac62157396c63839e80ea8f2326d0d8b
solikhin0433 <solikhinkhin645@gmail.com>
2026-09-09 08:37:14 +0700

    feat: add konfiguasi game

---
+++ src/constants/gameConfig.js
@@ -0,0 +1,25 @@
+// Timer sesi
+export const SESI_DURASI = 60 // detik
+
+// interval waktu tangkapan
+export const  MIN_TANGKAPAN_INTERVAL = 3000 // ms
+export const MAX_TANGKAPAN_INTERVAL = 6000 // ms
+
+// berat ikan
+export const MIN_BERAT_IKAN = 0.5 // kg
+export const MAX_BERAT_IKAN = 10 // kg
+export const BATAS_IKAN_BESAR = 7 // kg
+
+// durasi toast/ notifikasi tangkapan 
+export const DURASI_TOAST = 2000 // ms
+
+// pemain bot
+export const BOT_PEMAIN = [
+  { id: 'bot-1', name: 'Bot 1' },
+  { id: 'bot-2', name: 'Bot 2' },
+  { id: 'bot-3', name: 'Bot 3' },
+  { id: 'bot-4', name: 'Bot 4' },
+  { id: 'bot-5', name: 'Bot 5' },
+]
+
+export const JUMLAH_BOT = BOT_PEMAIN.length
